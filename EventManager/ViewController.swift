77d196f2d9ce5c0b349616201a12b173310182a3
Dmitrii Kim <mitya_kim@icloud.com>
2022-01-26 08:43:33 -0800

    Initial Commit

---
+++ EventManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventManager
+//
+//  Created by Mitya Kim on 1/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
